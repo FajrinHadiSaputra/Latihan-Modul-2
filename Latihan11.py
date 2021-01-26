29550fd338d689fcb9b641c57c716c5e042b1b4c
FajrinHadiSaputra <77818902+FajrinHadiSaputra@users.noreply.github.com>
2021-01-26 20:43:15 +0800

    fix latihan11

---
+++ Latihan11.py
@@ -0,0 +1,16 @@
+z=[2,3,4,5,6,7,8,9,0]
+y=[0,12,4,6,242,7,9]
+
+for z in z:
+    print(z)
+    if z==6:
+        break
+print('6 ditemukan, hentikan loop')
+
+print('')
+
+for y in y:
+    print(y)
+    if y==6:
+        break
+print('6 ditemukan, hentikan loop')
